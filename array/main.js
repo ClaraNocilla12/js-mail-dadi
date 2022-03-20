61bcf90d1460cc3b820d066305b64a25d5290efd
ClaraNocilla <claranocilla12@gmail.com>
2022-03-20 18:19:24 +0100

    mail

---
+++ array/main.js
@@ -0,0 +1,10 @@
+let array=[1,3,7,20,5,3,52,71];
+
+let i;
+
+for (i=length.array-5; i<length.array; i++){
+    console.log(array[i]);
+
+    document.getElementById("array").innerHTML+=`${array[i]}`;
+
+}
---
+++ mail/main.js
@@ -0,0 +1,17 @@
+let email=[`clara_nocilla@hotmail.com`,`claranocilla12@gmail.com`]
+
+let emailUtente=prompt("inserigni email");
+let i
+
+
+
+    for(i=0; i<email.length; i++){
+        if(emailUtente == email[i]){
+           
+    alert("email valida");
+        }else{
+            alert("email non valida");
+        }
+    }
+    
+    
